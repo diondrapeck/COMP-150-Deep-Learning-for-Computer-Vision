52d321427ceee7ded48df6d9f8986fa009d0269a
Diondra Peck <Diondra Peck>
2017-05-18 23:25:35 -0400

    Implement linear_svm.py - looped and vectorized

--- hw1/hw1/classifiers/linear_svm.py
+++ hw1/hw1/classifiers/linear_svm.py
@@ -14,14 +14,14 @@ def structured_loss_simple(W, X, y, reg):
   - loss as single float
   - gradient with respect to weights W; an array of same shape as W
   """
-  dW = np.zeros((X.T).shape) # initialize the gradient as zero
+  dW = np.zeros(W.shape) # initialize the gradient as zero
 
   # compute the loss and the gradient
-  num_classes = W.shape[1]
-  num_train = X.shape[0]
+  num_classes = W.shape[0]
+  num_train = X.shape[1]
   loss = 0.0
   for i in xrange(num_train):
-    scores = X[i].dot(W)
+    scores = W.dot(X[:,i])
     correct_class_score = scores[y[i]]
     for j in xrange(num_classes):
       if j == y[i]: # If correct class
@@ -31,6 +31,15 @@ def structured_loss_simple(W, X, y, reg):
         loss += margin
         dW[j, :] += X[:, i].T
         dW[y[i], :] -= X[:, i].T
+  
+  #############################################################################
+  # TODO:                                                                     #
+  # Compute the gradient of the loss function and store it dW.                #
+  # Rather that first computing the loss and then computing the derivative,   #
+  # it may be simpler to compute the derivative at the same time that the     #
+  # loss is being computed. As a result you may need to modify some of the    #
+  # code above to compute the gradient.                                       #
+  #############################################################################
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
@@ -43,16 +52,8 @@ def structured_loss_simple(W, X, y, reg):
   loss += 0.5 * reg * np.sum(W * W)
 
   # Add regularization to the gradient
-  dW += reg * W
-
-  #############################################################################
-  # TODO:                                                                     #
-  # Compute the gradient of the loss function and store it dW.                #
-  # Rather that first computing the loss and then computing the derivative,   #
-  # it may be simpler to compute the derivative at the same time that the     #
-  # loss is being computed. As a result you may need to modify some of the    #
-  # code above to compute the gradient.                                       #
-  #############################################################################
+  regularization = reg * W
+  dW += regulartization
 
   return loss, dW
 
@@ -82,7 +83,23 @@ def structured_loss_fast(W, X, y, reg):
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
+  
+  scores = np.dot(W, X) # also known as f(x_i, W)
 
+  correct_scores = np.ones(scores.shape) * scores[y, np.arange(0, scores.shape[1])]
+  deltas = np.ones(scores.shape)
+  L = scores - correct_scores + deltas
+
+  L[L < 0] = 0
+  L[y, np.arange(0, scores.shape[1])] = 0 # Don't count y_i
+  loss = np.sum(L)
+
+  # Average over number of training examples
+  num_train = X.shape[1]
+  loss /= num_train
+
+  # Add regularization
+  loss += 0.5 * reg * np.sum(W * W)
 
   #############################################################################
   # TODO:                                                                     #
@@ -94,6 +111,19 @@ def structured_loss_fast(W, X, y, reg):
   # loss.                                                                     #
   #############################################################################
 
+  grad = np.zeros(scores.shape)
+
+  L = scores - correct_scores + deltas
+
+  L[L < 0] = 0
+  L[L > 0] = 1
+  L[y, np.arange(0, scores.shape[1])] = 0 # Don't count y_i
+  L[y, np.arange(0, scores.shape[1])] = -1 * np.sum(L, axis=0)
+  dW = np.dot(L, X.T)
+
+  # Average over number of training examples
+  num_train = X.shape[1]
+  dW /= num_train
 
   #############################################################################
   #                             END OF YOUR CODE                              #
